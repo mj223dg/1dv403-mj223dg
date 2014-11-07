57c4a0bb779ae12a0dce1a9bd3f8d6c21e32aff5
mj223dg <mj223dg@student.lnu.se>
2014-11-07 15:58:04 +0100

    2014-11-07 15:57
    Done with Birthday, but need to change the error message.

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -4,15 +4,31 @@ window.onload = function(){
 
 	
 	var birthday = function(date){
+
+		//Hämtar dagens datum.
 	var currentdate = new Date();
+
+		//Användaren väljer sitt datum.
 	var myBirthday = new Date(date);
 
+			//Ful lösning på felmeddelande. Får vara där tills jag hittar annan lösning.
+		if (myBirthday > currentdate){
+		return (alert("Du är ej född."))
+	}
+	
+	myBirthday.setFullYear(currentdate.getFullYear());
 	if (currentdate.getTime() > myBirthday.getTime()) {
 		myBirthday.setFullYear(myBirthday.getFullYear()+1);
 	}
-	var diff = myBirthday.getTime()-currentdate.getTime();
-	var days = Math.floor(diff/(1000*60*60*24));
+	var diff = myBirthday.getTime() - currentdate.getTime();
+	var days = Math.ceil(diff/(1000*60*60*24));
 
+		//Om dagarna är lika med 365 returnera case 1.
+	if (days === 365) {
+	
+		return 0;
+	};
+	
 	return days; 
 
 
