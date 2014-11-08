e66bc8fa4faa177c83bea5d189d3827dd1925d7c
mj223dg <mj223dg@student.lnu.se>
2014-11-08 17:05:45 +0100

    2014-11-08 17:03
    Jobbar med vandalen. Går väl sakta frammåt. Mest en massa googlande.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,33 +1,37 @@
 "use strict";
 
 var makePerson = function(persArr) { 
-	var result = {
-		names,
-		minAge,
-		maxAge,
-		averageAge
-	};
+	var result = {};
+	var isString;
+	var isInteger;
+	var i;
 
-	var names;
-	var minAge;
-	var maxAge;
-	var averageAge;
+// Tar ut namnen och sorterar dem innan de sätts ihop igen.
+var names = persArr.map(function (name) {return name.name;}).sort(function(a, b); {return a.localCompare(b)}).join(", ");
 
+//Tar ut åldrarna
+var ages = persArr(function (age){return age.age;});
 
+//Lägger ihop åldrarna och tar fram medelåldern.
+var averageAge = persArr.reduce(function(a, b){ return a+b;});
+averageAge /= ages.length;
+averageAge = Math.round(averageAge)
+var maxAge = Math.max.apply(Math, ages);
+var minAge = Math.min.apply(Math, ages);
 
+// Lägger in resultaten i objektet.
+result.names = names;
+result.maxAge = maxAge;
+result.minAge = minAge;
+result.averageAge = averageAge;
 
-	// Lös uppgiften
-
+	
 	return result; 
 }; 
 
 var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+var data = data.map(function isString(value, index, array){
 
-var result = makePerson(data);
-
-
-
-console.log(result);
-
+names = names.every(function isString(value, index, array){
 
 
