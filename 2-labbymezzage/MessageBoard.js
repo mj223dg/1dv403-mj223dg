6c85b2ecc4026ab05abf2b04615e65facb6e56e4
mj223dg <mj223dg@student.lnu.se>
2014-11-20 01:50:22 +0100

    2014-11-20 01:50
    End of day

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -10,6 +10,34 @@ var MessageBoard = {
 	MessageBoard.messages.push("Hej, hej, nej, Nej");
 
 	console.log(MessageBoard.messages);
+	var submit = document.getElementById("button");
+
+	MessageBoard.messages.push("meddelande")
+
+
+	submit.onclick = function() {
+		alert("Blablabla");
 	}
+	var send = document.getElementById("sendMessage");
+	send.onclick = MessageBoard.createMessage;
+	},
+	createMessage: function(){
+		var empty = document.getElementById("meddelande").value;
+			if ("" === empty) {
+				return false;
+			};
+			var newMessage = new Message(document.form.meddelande.value);
+			MessageBoard.messages.push(newMessage);
+			MessageBoard.renderMessages();
+	}
+
+	var msg = document.createElement("div");
+	msg.className = "msg";
+	msg.innerHTML = MessageBoard.messages[messageID].getHTMLText();
+	msg.appendChild(button);
+
+var mess = document.getElementById("TextBoxContainer");
+mess.appendChild(msg);
 };
+
 window.onload = MessageBoard.init;
