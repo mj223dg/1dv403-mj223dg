773a7165a4921bc401c1215bc1a8019eea3956ca
mj223dg <mj223dg@student.lnu.se>
2014-11-25 19:26:02 +0100

    2014-11-25
    Working with task 5.

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -5,39 +5,17 @@ var MessageBoard = {
 
 	init:function(e){
 
-	var mess = new Message("test", new Date());
-
-	MessageBoard.messages.push("Hej, hej, nej, Nej");
-
-	console.log(MessageBoard.messages);
 	var submit = document.getElementById("button");
 
-	MessageBoard.messages.push("meddelande")
-
-
 	submit.onclick = function() {
-		alert("Blablabla");
+	var text = document.getElementById("textArea");
+
+	MessageBoard.messages.push( new Message(text.value));
+	console.log(MessageBoard.messages);
+
+
+
+		}
 	}
-	var send = document.getElementById("sendMessage");
-	send.onclick = MessageBoard.createMessage;
-	},
-	createMessage: function(){
-		var empty = document.getElementById("meddelande").value;
-			if ("" === empty) {
-				return false;
-			};
-			var newMessage = new Message(document.form.meddelande.value);
-			MessageBoard.messages.push(newMessage);
-			MessageBoard.renderMessages();
-	}
-
-	var msg = document.createElement("div");
-	msg.className = "msg";
-	msg.innerHTML = MessageBoard.messages[messageID].getHTMLText();
-	msg.appendChild(button);
-
-var mess = document.getElementById("TextBoxContainer");
-mess.appendChild(msg);
-};
-
+}
 window.onload = MessageBoard.init;
