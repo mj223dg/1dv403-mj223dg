814c91c66a85b2c5133cea73d5a9172505247aa3
mj223dg <mj223dg@student.lnu.se>
2014-11-27 23:25:05 +0100

    2014-11-27 23:24
    Done

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -1,5 +1,6 @@
 "Use strict";
 function Message(message, date){
+
 	this.getText = function(){
 		return message;
 	}
@@ -7,21 +8,48 @@ function Message(message, date){
 		message = text;
 	}
 	this.getDate = function(){
-		date = _date;
+		return date;
 	}
 	this.setDate = function(_date){
-
+		date = _date;
 	}
 }
 
 Message.prototype.toString = function(){
-	return this.getText()+" ("+this.getDate()+") ";
+
+	var date = this.getDate();
+	var month = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"]
+
+	var year = date.getFullYear();
+	var months = month[date.getMonth()];
+	var day = date.getDate();
+	
+	var hour = Message.zero(date.getHours());
+	var minute = Message.zero(date.getMinutes());
+	var seconds = Message.zero(date.getSeconds());
+
+	return "Inlägget skapades den: " + day + " " + months + " " + year + " " + "Klockan " + hour + ":" + minute + ":" + seconds;
 }
 
 Message.prototype.getHTMLText = function(){
 
+	var htmlText = this.getText();
+
+	return htmlText.replace(/[\n\r]/g, "<br />");
 }
 
 Message.prototype.getDateText = function(){
+	var date = this.getDate();
+	var hour = Message.zero(date.getHours());
+	var minute = Message.zero(date.getMinutes());
+	var seconds = Message.zero(date.getSeconds());
 
+	return hour + ":" + minute + ":" + seconds;
+}
+
+Message.zero = function(i) {
+	if ( i < 10 ){
+		i = "0" + i;
+	}
+	return i;
 }
--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -5,17 +5,92 @@ var MessageBoard = {
 
 	init:function(e){
 
-	var submit = document.getElementById("button");
+		var submit = document.getElementById("button");
+		button.addEventListener("click", MessageBoard.submit, false );
+		var text = document.getElementById("textArea");
+		text.addEventListener("keypress", function(e){
+			if(!e){ e = window.event; }
 
-	submit.onclick = function() {
-	var text = document.getElementById("textArea");
+			if(e.keyCode == 13 && !e.shiftKey){
+				e.preventDefault();
 
-	MessageBoard.messages.push( new Message(text.value));
-	console.log(MessageBoard.messages);
+				MessageBoard.submit();
+			}
+		});
+	},
+		
+	//Create message.
+	submit : function() {
+			var text = document.getElementById("textArea");
+			var mess = new Message(text.value, new Date());
+			MessageBoard.messages.push(mess);
+			var sentMessage = MessageBoard.messages.length - 1;
+			sentMessage.value = "";
+			MessageBoard.renderMessage(sentMessage); 
+			textArea.value = "";
+	},
 
+	renderMessage : function(messageID) {
+		var chatLog = document.getElementById("chatLog");
+		var p = document.createElement("p");
+		var time = document.createElement("p");
+		var messageText = document.createElement("p");
+		var div = document.createElement("div");
+		div.className = "divClass";
+		p.innerHTML = MessageBoard.messages[messageID].getHTMLText();
+		chatLog.appendChild(div);
+		div.appendChild(p);
+		div.appendChild(time);
+		div.appendChild(messageText);
 
+		time.innerHTML = MessageBoard.messages[messageID].getDateText();
+		
+		var a = document.createElement("a");
+		var img = document.createElement("img"); 
+		img.setAttribute("src", "delete.png");
+		div.appendChild(img);
 
+		var b = document.createElement("a");
+		var img2 = document.createElement("img");
+		img2.setAttribute("src", "clock.png");
+		div.appendChild(img2);
+
+		img.onclick = function() {
+
+			if (confirm("Vill du radera meddelandet?")){
+				MessageBoard.removeMessage(messageID);
+			}
 		}
+
+		img2.onclick = function() {
+			alert(MessageBoard.messages[messageID].toString());
+		}
+
+		MessageBoard.messageCounter();
+
+	},
+
+	renderMessages : function(messageID) {
+		document.getElementById("chatLog").innerHTML = "";
+	
+		for (var i = 0; i < MessageBoard.messages.length; ++i) {
+		MessageBoard.renderMessage(i);
+		}
+	},
+
+
+	removeMessage : function(messageID) {
+		MessageBoard.messages.splice(messageID, 1);
+		MessageBoard.renderMessages(messageID);
+		MessageBoard.messageCounter();
+	},
+
+	messageCounter : function() {
+		var messageCount = MessageBoard.messages.length;
+		var counter = document.getElementById("messageCounter");
+		counter.innerHTML = "Antal meddelande: " + messageCount;
 	}
-}
+
+	}
+
 window.onload = MessageBoard.init;
