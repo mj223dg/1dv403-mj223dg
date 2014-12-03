bd26fc92f95eb59ec6a482fa07c08da688f9ae82
mj223dg <mj223dg@student.lnu.se>
2014-12-03 15:35:19 +0100

    2014-12-03
    Still stuck....

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -13,13 +13,24 @@ var memory = {
 	imgArray: function (random){
 		console.log(random);
 		
-		for (var i = 0; i < random.length; i++){
-			var tablediv = document.getElementById("memoryContainer");
+		/*var tablediv = document.getElementById("memoryContainer");
 			var div = document.createElement("div");
 			var p = document.createElement("p");
 			div.className = "storePics";
 			memoryContainer.appendChild(div);
 			div.appendChild(p);
+			*/
+
+			var tablediv = document.getElementById("memoryContainer");
+			var li = document.createElement("li");
+			var a = document.createElement("a");
+			var text = document.createTextNode("123");
+			memoryContainer.appendChild(li);
+			li.appendChild(a);
+			a.appendChild(text);
+			a.setAttribute("pics/0.png")
+		for (var i = 0; i < random.length; i++){
+			
 			}
 		}
 	};
